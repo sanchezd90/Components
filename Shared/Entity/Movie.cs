ba604719f9c98d8d3ccf2f2c4ced2dbbe789f0ea
sanchezd90 <sanchezd90@gmail.com>
2021-09-27 11:09:01 -0300

    reestructuracion de componentes

--- Shared/Entity/Movie.cs
+++ Shared/Entity/Movie.cs
@@ -1,9 +1,10 @@
+using System.Text;
 using System;
 namespace Components.Shared.Entity
 {
     public class Movie
     {
-        public string Title { get; set; }
+        public string Title {get; set; }
         public string Description { get; set; }
         public string Genres { get; set; }
         public string MovieImage { get; set; }        
